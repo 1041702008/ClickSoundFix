bd37b06673a2daad0545791b91b5930f63d061ce
Celina <wittmarcel13@gmail.com>
2022-05-07 06:42:20 +0200

    Now only uses a Single Mixin Class without a timer, but only a simple active-check.

--- src/main/java/de/celinadev/clicksoundfix/Main.java
+++ src/main/java/de/celinadev/clicksoundfix/Main.java
@@ -1,12 +1,10 @@
 package de.celinadev.clicksoundfix;
 
-import de.celinadev.clicksoundfix.util.Util;
 import net.fabricmc.api.ModInitializer;
 
 public class Main implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        Util.startTimer();
     }
 }
--- src/main/java/de/celinadev/clicksoundfix/mixin/MixinSliderWidget.java
+++ src/main/java/de/celinadev/clicksoundfix/mixin/MixinSliderWidget.java
@@ -1,18 +1,33 @@
 package de.celinadev.clicksoundfix.mixin;
 
-import de.celinadev.clicksoundfix.util.Util;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.gui.widget.SliderWidget;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SliderWidget.class)
-public class MixinSliderWidget {
+public abstract class MixinSliderWidget extends ClickableWidget {
+
+    private boolean active = false;
+
+    protected MixinSliderWidget(int x, int y, int width, int height, Text message) {
+        super(x, y, width, height, message);
+    }
 
     @Inject(method = "onRelease", at = @At("HEAD"), cancellable = true)
-    private void cancelUISound(double mouseX, double mouseY, CallbackInfo ci) {
-        if (Util.SCREEN_OPEN_SINCE_TICKS < 5)
+    private void setInactive(double mouseX, double mouseY, CallbackInfo ci) {
+        boolean b = this.active;
+        this.active = false;
+        if (!b) {
             ci.cancel();
+        }
+    }
+
+    @Inject(method = "onClick", at = @At("HEAD"))
+    public void setActive(double mouseX, double mouseY, CallbackInfo ci) {
+        this.active = true;
     }
 }
